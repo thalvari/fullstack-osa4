252f5115bfddbebd4d6ce2b45c5495c85c223108
thalvari <tuomas.halvari@helsinki.fi>
2019-02-15 23:15:02 +0200

    -

---
+++ controllers/blogs.js
@@ -0,0 +1,25 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
+})
+
+blogsRouter.post('/', async (request, response, next) => {
+    const body = request.body
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    })
+    try {
+        const savedBlog = await blog.save()
+        response.json(savedBlog.toJSON())
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+module.exports = blogsRouter
